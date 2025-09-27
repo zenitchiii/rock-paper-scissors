9c0899702ccef60bf367b925a3721b855e1976fc
zenitchiii <105142307+zenitchiii@users.noreply.github.com>
2025-09-27 13:09:34 +0800

    choices

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hELLO, WORLD");
+function getComputerChoice() {
+    computer = Math.floor(Math.random() * 3) + 1;
+
+    if (computer === 1) {
+        return "rock";
+    } else if (computer === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
